2a20348b43e4fa591c55c783841a82ac85b5c619
caotin <caotin97@gmail.com>
2022-10-16 16:32:19 +0700

    audio visualize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [audio, setAudio] = useState(null)
+  const [audioData, setAudioData] = useState(null)
+  const [analyser, setAnalyser] = useState(null)
+  const [dataArray, setDataArray] = useState(null)
+  const [rafId, setRafId] = useState(null)
+
+  useEffect(() => {
+    const getMicrophone = async () => {
+      const audio = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false
+      });
+      setAudio(audio)
+    }
+    getMicrophone()
+  }, [])
+
+  useEffect(() => {
+
+    if (audio) {
+      const audioContext = new (window.AudioContext ||
+        window.webkitAudioContext)();
+      const analyser = audioContext.createAnalyser();
+      setDataArray(new Uint8Array(analyser.frequencyBinCount))
+      const source = audioContext.createMediaStreamSource(audio);
+      source.connect(analyser);
+      setRafId(requestAnimationFrame(tick))
+      setAnalyser(analyser)
+    }
+    return () => {
+      // cancelAnimationFrame(rafId);
+      // analyser.disconnect();
+      // source.disconnect();
+    }
+  }, [audio])
+
+  const tick = () => {
+    analyser.getByteTimeDomainData(dataArray)
+    setAudioData(dataArray)
+    setRafId(requestAnimationFrame(tick))
+  }
+
+  const count = audioData && Math.max(...audioData)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='audio-container'>
+        <div className='audio-box' style={{
+          border: 'solid',
+          borderWidth:  count / 15,
+          borderColor: 'pink'
+        }}></div>
+      </div>
     </div>
   );
 }
